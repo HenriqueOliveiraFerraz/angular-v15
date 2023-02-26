31a963b19f86ddea81f19a5fcb972139612e121b
HenriqueOliveiraFerraz <henrique.oliveira.ferraz@protonmail.com>
2023-02-26 18:53:21 -0300

    inter

--- projects/basic-template/src/app/core/interceptors/error.interceptor.ts
+++ projects/basic-template/src/app/core/interceptors/error.interceptor.ts
@@ -1,20 +1,35 @@
-import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
+  HttpErrorResponse,
   HttpEvent,
+  HttpHandler,
   HttpInterceptor,
+  HttpRequest,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { ErrorService } from '../services/error/error.service';
+import { NotificationService } from '../services/notification/notification.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(
+    private errorService: ErrorService,
+    private notificationService: NotificationService
+  ) {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        const errorMessage = this.errorService.getServerMessage(error, request);
+        const snackBarMessage = `${request.method}: ${error.message}`;
+        this.notificationService.showErrorSnackBar(snackBarMessage);
+        console.error(errorMessage);
+
+        return throwError(() => error);
+      })
+    );
   }
 }
